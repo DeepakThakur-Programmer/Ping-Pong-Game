da13c71c7cd54a3a24abd658bf2dbd7f10e023bd
Deepak_Thakur <61798279+DeepakThakur-Programmer@users.noreply.github.com>
2024-04-17 16:41:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+{
+    /* <script>
+            const div1 = document.getElementById("div1");
+            let msgHeight = "offsetHeight = " + div1.offsetHeight + "px";
+            let msgWidth = "offsetWidth = " + div1.offsetWidth + "px";
+            let msgTop = "offsetTop = " + div1.offsetTop + "px";
+            let msgLeft = "offsetLeft = " + div1.offsetLeft + "px";
+            console.log(msgHeight);
+            console.log(msgWidth);
+            console.log(msgTop);
+            console.log(msgLeft);
+        </script> */
+}
